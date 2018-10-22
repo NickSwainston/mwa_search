57686d48b52835285bb614228286f9327c1ca08e
NickSwainston <nickaswainston@gmail.com>
2018-10-22 21:22:01 +0800

    Neatened up splice wrapper

--- splice_wrapper.py
+++ splice_wrapper.py
@@ -33,7 +33,6 @@ with open('/group/mwaops/nswainston/code/blindsearch_scripts/obs_meta.csv', 'rb'
 
 obs_foun_check = False
 for omi in range(len(obsid_meta_file)):
-  print obsid, obsid_meta_file[omi][0]
   if int(obsid) == int(obsid_meta_file[omi][0]):
       print "getting obs metadata from obs_meta.csv"
       ob, ra, dec, duration, delays,centrefreq, channels =\
@@ -51,8 +50,6 @@ if not obs_foun_check:
 print "Obtaining metadata from http://mwa-metadata01.pawsey.org.au/metadata/ for OBS ID: " + str(obsid)
 beam_meta_data = meta.getmeta(service='obs', params={'obs_id':obsid})
 channels = beam_meta_data[u'rfstreams'][u"0"][u'frequencies']
-print channels
-
 
 chan_order=[]
 for i in range(24):
@@ -62,7 +59,7 @@ for i in range(24):
     #    chan_order.append(channels[i])
     #else:
     #    chan_order.append( channels[chan_st[-(channels[i] -128)]])
-print chan_order
+print "Chan order: {}".format(chan_order)
 
 #move into working fdir
 old_dir = os.getcwd()
@@ -77,9 +74,6 @@ submit_cmd = subprocess.Popen(submit_line,shell=True,stdout=subprocess.PIPE)
 out_lines = submit_cmd.stdout
 for l in out_lines:
     n_fits = l
-    print n_fits
-    if "incoh" in l:
-        print "check"
     
 if int(n_fits) == 0:
     new_bf = False
@@ -99,44 +93,24 @@ if int(n_fits) == 0:
 else:
     new_bf = True
 
-print n_fits
-
 for n in range(int(n_fits)):
     submit_line = submit_line_incoh = 'splice_psrfits '
     for ch in chan_order:
-            if ch < 10:
-                submit_line += '*'+str(obsid)+'_ch00'+str(ch)
-                submit_line_incoh += '*'+str(obsid)+'_ch00'+str(ch)
-            elif int(ch) < 100:
-                submit_line += '*'+str(obsid)+'_ch0'+str(ch)
-                submit_line_incoh += '*'+str(obsid)+'_ch0'+str(ch)
-            else:
-                submit_line += '*'+str(obsid)+'_ch'+str(ch)
-                submit_line_incoh += '*'+str(obsid)+'_ch'+str(ch)
-            if (int(n)+1) < 10:
-                submit_line += '_000'+str(int(n)+1)+'.fits '
-                submit_line_incoh += '_incoh_000'+str(int(n)+1)+'.fits '
-            else:
-                submit_line += '_00'+str(int(n)+1)+'.fits '
-                submit_line_incoh += '_incoh_00'+str(int(n)+1)+'.fits '
+        submit_line += '*{}_ch{:03d}_{:04d}.fits '.format(obsid, ch, int(n)+1)
+        submit_line_incoh +=  '*{}_ch{:03d}_incoh_{:04d}.fits '.format(obsid, ch, int(n)+1)
     submit_line += 'temp_'+str(int(n)+1)
     submit_line_incoh += 'temp_incoh_'+str(int(n)+1)
     
-
     if args.incoh:
         print submit_line_incoh
         submit_cmd = subprocess.Popen(submit_line_incoh,shell=True,stdout=subprocess.PIPE)
         out_lines = submit_cmd.stdout        
         for l in out_lines:
-            print l
+            print l,
         time.sleep(5)
-        if n < 9:
-            print 'temp_incoh_'+str(int(n)+1)+'_0001.fits'
-            os.rename('temp_incoh_'+str(int(n)+1)+'_0001.fits',str(obsid)+'_incoh_000'+\
-                      str(int(n)+1)+'.fits')
-        else:
-            os.rename('temp_incoh_'+str(int(n)+1)+'_0001.fits', str(obsid)+'_incoh_00'+\
-                      str(int(n)+1)+'.fits')
+        print 'temp_incoh_'+str(int(n)+1)+'_0001.fits', '{}_incoh_{:04d}.fits'.format(obsid, int(n)+1)
+        os.rename('temp_incoh_'+str(int(n)+1)+'_0001.fits',
+                  '{}_incoh_{:04d}.fits'.format(obsid, int(n)+1))
         
         #wait to get error code
         (output, err) = submit_cmd.communicate()  
@@ -152,15 +126,11 @@ for n in range(int(n_fits)):
     submit_cmd = subprocess.Popen(submit_line,shell=True,stdout=subprocess.PIPE)
     out_lines = submit_cmd.stdout
     for l in out_lines:
-        print l
+        print l,
     time.sleep(5)
-    if n < 9:
-        print 'temp_'+str(int(n)+1)+'_0001.fits'
-        os.rename('temp_'+str(int(n)+1)+'_0001.fits',str(obsid)+'_000'+str(int(n)+1)+'.fits')
-        #if not os.path.isfile(str(obsid)+'_0011.fits'):
-        #    os.rename('temp_'+str(int(n)+1)+'_0001.fits',str(obsid)+'_0011.fits')
-    else:
-        os.rename('temp_'+str(int(n)+1)+'_0001.fits', str(obsid)+'_00'+str(int(n)+1)+'.fits')
+    print 'temp_'+str(int(n)+1)+'_0001.fits', '{}_{:04d}.fits'.format(obsid, int(n)+1)
+    os.rename('temp_'+str(int(n)+1)+'_0001.fits',
+              '{}_{:04d}.fits'.format(obsid, int(n)+1))
     
     #wait to get error code
     (output, err) = submit_cmd.communicate()  
