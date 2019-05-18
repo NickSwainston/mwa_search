83093d72ff1ec2d417f8f3d3a458963c1bdcb60b
NickSwainston <nickaswainston@gmail.com>
2019-05-18 22:04:39 +1000

    Removed unused scripts from the repo

--- test_blindsearch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#changes the PATH and PYTHON PATH so it use the scripts in the main directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo $DIR
-export PATH="$( echo $PATH| tr : '\n' |grep -v ${DIR}/bin/ | paste -s -d: )"
-export PATH=${PATH}:${DIR}/
-export PYTHONPATH="$( echo $PATH| tr : '\n' |grep -v ${DIR}/bin/ | paste -s -d: )"
-export PYTHONPATH=${PATH}:${DIR}/
-
