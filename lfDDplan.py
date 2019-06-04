c37d7beb4e9230a3955d42d89279403369ccefe6
NickSwainston <nickaswainston@gmail.com>
2019-06-04 15:23:44 +1000

    Made lfDDplan.py follow Prestos calculations closers

--- lfDDplan.py
+++ lfDDplan.py
@@ -9,18 +9,32 @@ def dd_plan(centrefreq, bandwidth, nfreqchan, timeres, lowDM, highDM):
     DD_plan_array = []
     freqres = bandwidth / float(nfreqchan)
     previous_DM = lowDM
+
+    #number of time samples smeared over before moving to next D_dm
+    smear_fact = 2.
+
     #Loop until you've made a hit your range max
     D_DM = 0.
     while D_DM < highDM:
         #calculate the DM where the current time resolution equals the 
         #dispersion in a frequency channel (a bit of an overkill)
-        D_DM = timeres * centrefreq**3 /\
+        
+        #Dm smear over a frequency channel
+        dm_smear = previous_DM * freqres * 8.3 * 10.**6 / centrefreq**3 
+        total_smear = math.sqrt(timeres**2 +
+                                dm_smear**2)
+                                
+        
+        D_DM = smear_fact * timeres * centrefreq**3 /\
                (8.3 * 10.**6 * freqres)
         
         #difference in DM that will double the effective width (eq 6.4 of pulsar handbook)
         #TODO make this more robust
-        DM_step = math.sqrt( (2.*timeres)**2 - timeres**2 )/\
-                  (8.3 * 10**6 * bandwidth / centrefreq**3)
+        #DM_step = math.sqrt( (2.*timeres)**2 - timeres**2 )/\
+        #          (8.3 * 10**6 * bandwidth / centrefreq**3)
+        DM_step = smear_fact * total_smear * centrefreq**3 /\
+                  (8.3 * 10.**6 * 0.5 * bandwidth) 
+
 
         #round to nearest 0.01
         DM_step = round(DM_step, 2)
@@ -30,15 +44,12 @@ def dd_plan(centrefreq, bandwidth, nfreqchan, timeres, lowDM, highDM):
 
         if D_DM > highDM:
             #last one so range from to max
-            nDM_step = int((highDM - previous_DM) / DM_step)
-            DD_plan_array.append([ previous_DM, highDM, DM_step, nDM_step, timeres ])
-            previous_DM = D_DM
-        elif D_DM > previous_DM:
-            #range from last to new
-            nDM_step = int((D_DM - previous_DM) / DM_step)
-            DD_plan_array.append([ previous_DM, D_DM, DM_step, nDM_step, timeres ])
-            previous_DM = D_DM
-        #else don't do anything because it's under your low DM
+            D_DM = highDM
+        #range from last to new
+        D_DM = round(D_DM, 2)
+        nDM_step = int((D_DM - previous_DM) / DM_step)
+        DD_plan_array.append([ previous_DM, D_DM, DM_step, nDM_step, timeres ])
+        previous_DM = D_DM
 
         #Double time res to account for incoherent dedispersion
         timeres *= 2.
