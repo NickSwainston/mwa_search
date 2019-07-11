36167e8406393534269b006a530c291bea83981e
keegansmith21 <keegan.r.s21@gmail.com>
2019-07-11 15:38:31 +0800

    undoing changes

--- mwa_search_pipeline.py
+++ mwa_search_pipeline.py
@@ -298,12 +298,12 @@ def dependant_splice_batch(obsid, pointing, product_dir, pointing_dir, job_id_li
         #check_known_pulsars.py uses this to check if it was detected and if so upload it
         commands.append('cd {0}'.format(pointing_dir))
            
-            for pulsar in pulsar_list 
+        for pulsar in pulsar_list 
             #load presto module here because it uses python 2
             commands.append('echo "Folding on known pulsar"'.format(pulsar))
             commands.append('psrcat -e {0} > {0}.eph'.format(pulsar))
             commands.append("sed -i '/UNITS           TCB/d' {0}.eph".format(pulsar))
-            commands.append("prepfold -o {0} -noxwin -runavg -noclip -timing {1}.eph -nsub 256 {2}/1*fits -n {3}".format(obsid, pulsar, pointing_dir, nbins))
+            commands.append("prepfold -o {0} -noxwin -runavg -noclip -timing {1}.eph -nsub 256 {2}/1*fits".format(obsid, pulsar, pointing_dir))
             commands.append('errorcode=$?')
             commands.append('pulsar={}'.format(pulsar[1:]))
             pulsar_bash_string = '${pulsar}'
@@ -311,7 +311,7 @@ def dependant_splice_batch(obsid, pointing, product_dir, pointing_dir, job_id_li
             #causes an error with -timing but not -psr
             commands.append('if [ "$errorcode" != "0" ]; then')
             commands.append('   echo "Folding using the -psr option"')
-            commands.append('   prepfold -o {0} -noxwin -runavg -noclip -psr {1} -nsub 256 {2}/1*fits'.format(obsid, pulsar, pointing_dir, nbins))
+            commands.append('   prepfold -o {0} -noxwin -runavg -noclip -psr {1} -nsub 256 {2}/1*fits'.format(obsid, pulsar, pointing_dir))
             commands.append('   pulsar={}'.format(pulsar))
             commands.append('fi')
             commands.append('rm {0}.eph'.format(pulsar))
