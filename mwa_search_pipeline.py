30fb0398141bd550c53858c9ae1c0487384012b6
keegansmith21 <keegan.r.s21@gmail.com>
2019-07-19 10:41:24 +0800

    Changed multibeam function to accommodate the 'search opts' class

--- mwa_search_pipeline.py
+++ mwa_search_pipeline.py
@@ -364,28 +364,29 @@ def process_vcs_wrapper(search_opts, pointings,
             dependant_splice_batch(search_opts, job_id_list=job_id_list, pulsar_list=pulsar_list)
     return
 
-def multibeam_binfind(run_params, job_id_list)
+def multibeam_binfind(search_opts, job_id_list, threshold, pulsar, loglvl="INFO", vcs_tools="master"):
     """
     Takes many pointings and launches data_processing_pipeline which folds on all of the pointings and finds the best one. This will by default continue running the processing pipeline
     """
     pointing_str = ""
-    for pointing in run_params.pointing_dir:
+    for pointing in search_opts.pointing_dir:
         pointing_str = pointing_str + pointing + " "
 
     commands = []
     commands.append("echo 'Folding on multiple pointings'")
-    commands.append("data_process_pipeline.py -m m -d {0} -o {1} -O {2} -p {3} -t {4} -L {5}"\
-                    .format(pointing_str, run_params.obsid, run_params.cal_id, run_params.pulsar,\
-                    run_params.threshold, run_params.loglvl))
+    commands.append("data_process_pipeline.py -m m -d {0} -o {1} -O {2} -p {3} -t {4} -L {5} --mwa_search {6}\
+                    --vcs_tools {7}"\
+                    .format(pointing_str, search_opts.obsid, search_opts.cal_id, pulsar,\
+                    threshold, loglvl, search_opts.search_ver, vcs_tools))
     
-    name="multibeam_fold_{0}_{1}".format(run_params.pulsar, run_params.obsid)
-    batch_dir("/group/mwaops/vcs/{0}/batch".format(run_params.obsid))
+    name="multibeam_fold_{0}_{1}".format(pulsar, search_opts.obsid)
+    batch_dir("/group/mwaops/vcs/{0}/batch".format(search_opts.obsid))
     submit_slurm(name, commands,\
                 batch_dir=batch_dir,\
                 slurm_kwargs={"time": "00:05:00"},\
-                module_list=['mwa_search/{0}'.format(run_params.mwa_search),\
+                module_list=['mwa_search/{0}'.format(search_opts.mwa_search),\
                               'presto/no-python'],\
-                submit=True, vcstools_version=run_params.vcs_tools,\
+                submit=True, vcstools_version=vcs_tools,\
                 depend=job_id_list)
      
 
