07fa0694edb0ac4cd835f3275c059eb06af423ba
keegansmith21 <keegan.r.s21@gmail.com>
2020-01-30 14:23:51 +0800

    Increased time allocation for moving pointing dirs as It runs out for large obs

--- scripts/binfinder.py
+++ scripts/binfinder.py
@@ -301,7 +301,7 @@ def submit_to_db_and_continue(run_params, best_bins):
 
     dep_id = submit_slurm(name, commands,\
                  batch_dir=batch_dir,\
-                 slurm_kwargs={"time": "01:00:00"},\
+                 slurm_kwargs={"time": "03:00:00"},\
                  module_list=['mwa_search/{0}'.format(run_params.mwa_search)],\
                  submit=True, vcstools_version="{0}".format(run_params.vcs_tools))
 
