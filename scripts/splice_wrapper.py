c4ccbb2bb438f3bcf566b25f195dea57d8efbe7c
NickSwainston <nickaswainston@gmail.com>
2020-03-31 15:00:00 +1100

    Removed the SSD features as they're not need. Added an input file check (not throughly tested), now handles picket fence data and outputs the files with a more verbose name

--- scripts/splice_wrapper.py
+++ scripts/splice_wrapper.py
@@ -7,6 +7,7 @@ import time
 import mwa_metadb_utils as meta
 import glob
 from shutil import copy
+from itertools import groupby, count
 
 parser = argparse.ArgumentParser(description="""
 Wraps the splice_psrfits.sh script to automate it. Should be run from the foulder containing the files.
@@ -36,88 +37,97 @@ else:
     channels = beam_meta_data[u'rfstreams'][u"0"][u'frequencies']
 print("Chan order: {}".format(channels))
 
-#hostname = socket.gethostname()
-hostname = 'temp'
-if hostname.startswith('john') or hostname.startswith('bryan'):
-    #If on ozstar use their SSD to improve I/O
-    SSD_file_dir = '{}/'.format(os.environ['JOBFS'])
-    print(SSD_file_dir)
-else:
-    SSD_file_dir = ''
-
 
 # Move into working dir
 old_dir = os.getcwd()
-if hostname.startswith('john') or hostname.startswith('bryan'):
-    os.chdir(SSD_file_dir)
-else:
+if args.work_dir:
     os.chdir(args.work_dir)
 
 
 # Getting number of files list
-if args.incoh:
-    n_fits_file = glob.glob('{}/*{}_incoh_ch{:03d}_*fits'.format(args.work_dir, obsid, channels[-1]))
-else:
-    n_fits_file = glob.glob('{}/*{}*_ch{:03d}_*fits'.format(args.work_dir, obsid, channels[-1]))
-n_fits = []
-for file_name in n_fits_file:
-    n_fits.append(int(file_name[-9:-5]))
-n_fits.sort()
-print('Fits number order: {}'.format(n_fits))
-
-for n in n_fits:
-    # List unspliced files
-    unspliced_files = []
-    for ch in channels:
-        if args.incoh:
-            unspliced_files.append(glob.glob('{}/*{}_incoh_ch{:03d}_{:04d}.fits'.format(args.work_dir,
-                                                   obsid, ch, n))[0])
-        else:
-            unspliced_files.append(glob.glob('{}/*{}*_ch{:03d}_{:04d}.fits'.format(args.work_dir,
-                                                  obsid, ch, n))[0])
-
-
-    if hostname.startswith('john') or hostname.startswith('bryan'):
-        print("Moving each channel onto $JOBFS")
-        for us_file in unspliced_files:
-            copy(us_file, SSD_file_dir)
-
-    # Create splice command and submit
-    submit_line = 'splice_psrfits '
-    for us_file in unspliced_files:
-        if hostname.startswith('john') or hostname.startswith('bryan'):
-            file_on_SSD = "{}{}".format(SSD_file_dir, us_file.replace(args.work_dir, ''))
-            submit_line += '{} '.format(file_on_SSD)
-        else:
-            submit_line += '{} '.format(us_file)
-    submit_line += '{}temp_{}'.format(SSD_file_dir, n)
-
-    print(submit_line)
-    submit_cmd = subprocess.Popen(submit_line,shell=True,stdout=subprocess.PIPE)
-    out_lines = submit_cmd.stdout
-    for l in out_lines:
-        print(l.decode()[:-1])
-    time.sleep(1)
-
-
-    print('Finished {}_{:04d}.fits'.format(obsid, n))
-    if hostname.startswith('john') or hostname.startswith('bryan'):
-        #Use copy because it's faster than mv
-        copy('{}temp_{}_0001.fits'.format(SSD_file_dir, n),
-             '{}/{}_{:04d}.fits'.format(args.work_dir, obsid, n))
-        ssd_files = glob.glob('{}/*'.format(SSD_file_dir))
-        for sf in ssd_files:
-            os.remove(sf)
+n_fits_list = []
+fits_len = []
+for ch in channels:
+    if args.incoh:
+        fits_glob = glob.glob('{}/*{}_incoh_ch{:03d}_*fits'.format(args.work_dir, obsid, ch))
     else:
-        os.rename('temp_{}_0001.fits'.format(n), '{}_{:04d}.fits'.format(obsid, n))
+        fits_glob = glob.glob('{}/*{}*_ch{:03d}_*fits'.format(args.work_dir, obsid, ch))
+    n_fits = []
+    for file_name in fits_glob:
+        n_fits.append(int(file_name[-9:-5]))
+    n_fits.sort()
+    n_fits_list.append(n_fits)
+    fits_len.append(len(n_fits))
+print('Total fits number: {}'.format(max(fits_len)))
 
-    #wait to get error code
-    (output, err) = submit_cmd.communicate()
-    p_status = submit_cmd.wait()
+# Grab pointing from unspliced files
+if args.incoh:
+    pointing = 'incoh'
+else:
+    pointing = "{0}_{1}".format(fits_glob[0].split("_")[2], fits_glob[0].split("_")[3])
 
-    print("exit code: " + str(submit_cmd.returncode))
-    if args.delete and int(submit_cmd.returncode) == 0:
+# Check all files are there
+max_fits_order = n_fits_list[fits_len.index(max(fits_len))]
+missing_files = []
+for ch in channels:
+    for file_num in max_fits_order:
+        if args.incoh:
+            if not glob.glob('{}/*{}_incoh_ch{:03d}_{:04d}.fits'.format(args.work_dir, obsid, ch, file_num)):
+                missing_files.append(glob.glob('{}/*{}_incoh_ch{:03d}_{:04d}.fits'.format(args.work_dir, obsid, ch, file_num)))
+        else:
+            if not glob.glob('{}/*{}*_ch{:03d}_{:04d}.fits'.format(args.work_dir, obsid, ch, file_num)):
+                missing_files.append(glob.glob('{}/*{}*_ch{:03d}_{:04d}.fits'.format(args.work_dir, obsid, ch, file_num)))
+if len(missing_files) != 0:
+    print("Missing the following files:")
+    for mf in missing_files:
+        print(mf[0])
+    print("Exiting")
+    sys.exit(1)
+
+print('Fits number order: {}'.format(max_fits_order))
+
+# Put channels into consecutive lists to properly splice picket fence data
+con_channels = []
+for _, g in groupby(channels, key=lambda n, c=count(): n-next(c)):
+    con_channels.append(list(g))
+
+for n in max_fits_order:
+    for channels in con_channels:
+        # List unspliced files
+        unspliced_files = []
+        for ch in channels:
+            if args.incoh:
+                unspliced_files.append(glob.glob('{}/*{}_incoh_ch{:03d}_{:04d}.fits'.format(args.work_dir,
+                                                    obsid, ch, n))[0])
+            else:
+                unspliced_files.append(glob.glob('{}/*{}*_ch{:03d}_{:04d}.fits'.format(args.work_dir,
+                                                    obsid, ch, n))[0])
+
+        # Create splice command and submit
+        submit_line = 'splice_psrfits '
         for us_file in unspliced_files:
-            print("Deleting: " + str(us_file))
-            if os.path.isfile(us_file):
-                 os.remove(us_file)
+            submit_line += '{} '.format(us_file)
+        submit_line += 'temp_{}'.format(n)
+
+        print(submit_line)
+        submit_cmd = subprocess.Popen(submit_line,shell=True,stdout=subprocess.PIPE)
+        out_lines = submit_cmd.stdout
+        for l in out_lines:
+            print(l.decode()[:-1])
+        time.sleep(1)
+
+
+        print('Finished {}_{}_ch{:03d}-{:03d}_{:04d}.fits'.format(obsid, pointing, min(channels), max(channels), n))
+        os.rename('temp_{}_0001.fits'.format(n),
+                  '{}_{}_ch{:03d}-{:03d}_{:04d}.fits'.format(obsid, pointing, min(channels), max(channels), n))
+
+        #wait to get error code
+        (output, err) = submit_cmd.communicate()
+        p_status = submit_cmd.wait()
+
+        print("exit code: " + str(submit_cmd.returncode))
+        if args.delete and int(submit_cmd.returncode) == 0:
+            for us_file in unspliced_files:
+                print("Deleting: " + str(us_file))
+                if os.path.isfile(us_file):
+                    os.remove(us_file)
