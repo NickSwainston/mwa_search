7c23c5bd605c79f95b81829848e25d80a5ecf4c6
keegansmith21 <keegan.r.s21@gmail.com>
2020-07-16 11:43:47 +0800

    Black format changes

--- dpp/pulsar_obs_helper.py
+++ dpp/pulsar_obs_helper.py
@@ -2,6 +2,7 @@
 import find_puslar_in_obs as fpio
 import sn_flux_est as snfe
 
+
 def find_fold_times(pulsar, obsid, beg, end, min_z_power=(0.3, 0.1)):
     """
     Finds the fractional time the pulsar is in the beam at some zenith normalized power
@@ -35,19 +36,24 @@ def find_fold_times(pulsar, obsid, beg, end, min_z_power=(0.3, 0.1)):
 
     min_z_power = sorted(min_z_power, reverse=True)
     names_ra_dec = fpio.grab_source_alog(pulsar_list=[pulsar])
-    pow_dict, _ = find_pulsars_power(obsid, powers=min_z_power, names_ra_dec=names_ra_dec)
+    pow_dict, _ = find_pulsars_power(
+        obsid, powers=min_z_power, names_ra_dec=names_ra_dec
+    )
     for power in pow_dict.keys():
         psr_list = pow_dict[power][obsid]
         enter = None
         leave = None
-        if psr_list: #if pulsar is in beam for this power coverage
-            this_enter, this_leave = snfe.pulsar_beam_coverage(obsid, pulsar, beg=beg, end=end, min_z_power=power)
+        if psr_list:  # if pulsar is in beam for this power coverage
+            this_enter, this_leave = snfe.pulsar_beam_coverage(
+                obsid, pulsar, beg=beg, end=end, min_z_power=power
+            )
             if this_enter is not None and this_leave is not None:
                 enter = this_enter
                 leave = this_leave
                 break
 
     return [enter, leave, power]
+
 
 def find_pulsars_power(obsid, powers=None, names_ra_dec=None):
     """
@@ -76,15 +82,17 @@ def find_pulsars_power(obsid, powers=None, names_ra_dec=None):
     if not powers:
         powers = [0.3, 0.1]
     elif not (isinstance(powers, list) or isinstance(powers, tuple)):
-        #try this if powers isn't iterable
-        powers=list(powers)
+        # try this if powers isn't iterable
+        powers = list(powers)
 
     if names_ra_dec is None:
         names_ra_dec = np.array(fpio.grab_source_alog(max_dm=250))
 
     pulsar_power_dict = {}
     for pwr in powers:
-        obs_data, meta_data = fpio.find_sources_in_obs([obsid], names_ra_dec, dt_input=100, min_power=pwr)
+        obs_data, meta_data = fpio.find_sources_in_obs(
+            [obsid], names_ra_dec, dt_input=100, min_power=pwr
+        )
         pulsar_power_dict[pwr] = obs_data
 
     return pulsar_power_dict, meta_data
