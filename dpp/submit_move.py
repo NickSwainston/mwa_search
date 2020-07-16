b4eee197b1c08a74fa82dd9b1472a95a33b08db9
keegansmith21 <keegan.r.s21@gmail.com>
2020-07-16 11:43:59 +0800

    Created the move function

--- dpp/submit_move.py
+++ dpp/submit_move.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+from os import path.join as ospj
+
 from binfinder import NoSuitableProfileError, bestprof_info
 from plotting_toolkit import plot_bestprof
+from data_processing_pipeline import resubmit_self
+from config_vcs import load_config_file
 
-def submit_to_db(pipe):
+def submit_to_db(pipe, dep_id=None, dep_type="afterok"):
     """Submits the best fold profile to the pulsar database. Will also submit .ppps, .png and .pfd"""
     bin_list = pipe["folds"]["init"].keys().append(pipe["folds"]["post"].keys())
     commands = []
@@ -17,45 +21,68 @@ def submit_to_db(pipe):
         png = glob.glob(os.path.join(pipe["run_ops"]["my_dir"], png))[0]
         pfd = f"*_b{bin_count}*_{pipe['source']['name'][1:]}*.pfd".format(best_bins)
         pfd = glob.glob(os.path.join(pipe["run_ops"]["my_dir"], pfd))[0]
-        #Make a nice plot
-        plotting_toolkit.plot_bestprof(os.path.join(pipe["run_ops"]["my_dir"], bestprof, out_dir=pipe["run_ops"]["my_dir"])
         commands.append(f"echo 'Submitting profile to database with {bin_count} bins'")
-        commands.append(f"submit_to_database.py -o {pipe['obs']['id']} --cal_id {pipe['obs']['cal_id']} -p {pipe['source']['name']} --bestprof {bestprof} --ppps {ppps}"
+        commands.append(f"submit_to_database.py -o {pipe['obs']['id']} --cal_id {pipe['obs']['cal_id']} -p {pipe['source']['name']} --bestprof {bestprof} --ppps {ppps}")
+        # Also Make a nice plot
+        plot_bestprof(os.path.join(pipe["run_ops"]["my_dir"], bestprof, out_dir=pipe["run_ops"]["my_dir"]))
 
-    #submit job
     name = f"Submit_db_{pipe['source']['name']}_{pipe['obs']['id']}"
     batch_dir = os.path.join(comp_config['base_product_dir'], pipe['obs']['id'], "batch")
-    dep_id = submit_slurm(name, commands,
+    this_id = submit_slurm(name, commands,
                 batch_dir=batch_dir,
-                slurm_kwargs={"time": "04:00:00"},
-                depend=run_params.stokes_dep,
+                slurm_kwargs={"time": "02:00:00"},
+                depend=dep_id,
                 module_list=[f"mwa_search/{pipe['run_ops']['mwa_search']}", f"mwa_search/{pipe['run_ops']['vcstools']}"],
-                submit=True)
+                submit=True, depend_type=dep_type)
 
     logger.info(f"Submission script on queue for profile: {bestprof}")
     logger.info(f"Job name: {name}")
-    logger.info(f"Job ID: {dep_id}")
+    logger.info(f"Dependenices: {dep_id}")
+    logger.info(f"Depend type: {dep_type}")
+    logger.info(f"Job ID: {this_id}")
 
-    return dep_id
+    return this_id
 
-def move_product_dir(pipe)
+def move_product_dir(pipe, dep_id=None, dep_type="afterok"):
+    """Submits a job to move the pointing directory from /astro to /group"""
+    current_dir = pipe["run_ops"]["dir"]
+    move_loc = ospj(load_from_config()["base_product_dir"], pipe["obs"]["id"], "data_products")
+    pointing = [i for i in current_dir.split("/") if i != ""]
+    new_dir = os.path.join(move_loc, pointing[-1])
 
+    commands = []
+    commands.append(f"echo 'Moving {current_dir} to new location: {new_dir}'")
     if run_params.pulsar[-1].isalpha():
         commands.append("cp -ru {0} {1}".format(run_params.pointing_dir, new_pointing_dir))
     else:
         commands.append("mv {0} {1}".format(run_params.pointing_dir, new_pointing_dir))
 
+    name = f"Move_{pipe['source']['name']}_{pipe['obs']['id']}"
+    batch_dir = os.path.join(comp_config['base_product_dir'], pipe['obs']['id'], "batch")
+    this_id = submit_slurm(name, commands,
+                batch_dir=batch_dir,
+                slurm_kwargs={"time": "02:00:00"},
+                depend=dep_id,
+                submit=True, depend_type=dep_type)
+
+    logger.info(f"Move script on queue for pointing directory: {current_dir}")
+    logger.info(f"Job name: {name}")
+    logger.info(f"Dependenices: {dep_id}")
+    logger.info(f"Depend type: {dep_type}")
+    logger.info(f"Job ID: {this_id}")
+
 
 def fill_pipe_folds(pipe):
-    """Fills the pipe with thje bestprof information from all available folds"""
+    """Fills the pipe with the bestprof information from all available folds"""
     for bins in pipe["folds"]["init"]:
         bestprof = glob.glob(f"*b{bins}**{pipe['source']['name'][1:]}*.bestprof")[0]
-        info = bestprof_info(filename=bestprof)
+        info = bestprof_info(bestprof)
         pipe["folds"]["init"][bins] = info
     for bins in pipe["folds"]["post"]:
         bestprof = glob.glob(f"*b{bins}**{pipe['source']['name'][1:]}*.bestprof")[0]
-        info = bestprof_info(filename=bestprof)
+        info = bestprof_info(bestprof)
         pipe["folds"]["post"][bins] = info
+
 
 def get_best_fold(pipe):
     """Finds the profile with the highest bins with acceptable SN and Chi values in a directory"""
@@ -93,3 +120,6 @@ def get_best_fold(pipe):
     def submit_move_main(pipe):
         fill_pipe_folds(pipe)
         get_best_fold(pipe)
+        dep_id = submit_to_db(pipe)
+        dep_id = move_product_dir(pipe, dep_id=dep_id, dep_type="afterok")
+        resubmit_self(pipe, dep_id=dep_id, dep_type="afterok")
