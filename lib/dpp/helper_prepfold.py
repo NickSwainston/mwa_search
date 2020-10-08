83dc1daee2f40e8f7dfb17fb0a7cfb49c0e69741
keegansmith21 <keegan.r.s21@gmail.com>
2020-10-08 15:20:30 +0800

    Created unique names for the .sh files to avoid overwriting

--- lib/dpp/helper_prepfold.py
+++ lib/dpp/helper_prepfold.py
@@ -61,9 +61,9 @@ def add_prepfold_to_commands(prep_kwargs, eph=None, eph_name=None):
     return commands
 
 
-def write_cmd_to_file(pipe, commands):
+def write_cmd_to_file(pipe, commands, name):
     """Writes the prepfold command to a text file"""
-    with open(f"prepfold_cmd_{pipe['run_ops']['file_precursor']}.sh", 'w') as f:
+    with open(name, 'w') as f:
         for cmd in commands:
             f.write(cmd)
 
@@ -86,6 +86,7 @@ def prepfold_cmd_make_main(kwargs):
     for bin_count in folds:
         prep_kwargs = common_kwargs(pipe, bin_count)
         cmd = add_prepfold_to_commands(prep_kwargs, pipe["source"]["edited_eph"], pipe["source"]["edited_eph_name"])
-        write_cmd_to_file(pipe, cmd)
+        name = f"prepfold_cmd_{pipe['run_ops']['file_precursor']}_{bin_count}b.sh"
+        write_cmd_to_file(pipe, cmd, name)
     #update yaml file
     dump_to_yaml(pipe, label=kwargs["label"])
