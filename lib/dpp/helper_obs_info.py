d90bcbca60aa15961777429bb99d0bc96470bf54
keegansmith21 <keegan.r.s21@gmail.com>
2020-10-23 14:51:39 +0800

    Created a python script to create commands and read the ouput of polarimetry commands to hand to nectflow pipeline

--- lib/dpp/helper_obs_info.py
+++ lib/dpp/helper_obs_info.py
@@ -260,7 +260,6 @@ def get_sources_in_fov(obsid, source_type, fwhm):
     """
     names_ra_dec = fpio.grab_source_alog(source_type=source_type)
     obs_data, meta_data = fpio.find_sources_in_obs([obsid], names_ra_dec, dt_input=100)
-
     name_list = []
     pointing_list = []
     for pulsar_line in obs_data[obsid]:
