9c80dbce01540d49e70325019d022d082d9a9947
keegansmith21 <keegan.r.s21@gmail.com>
2021-01-12 16:14:41 +0800

    Updated how some files are made

--- lib/dpp/helper_RM.py
+++ lib/dpp/helper_RM.py
@@ -2,9 +2,10 @@
 import logging
 from os.path import join
 
+from rm_synthesis import rm_synth_main
 from vcstools.prof_utils import subprocess_pdv, get_from_ascii, auto_gfit
 from dpp.helper_archive import generate_archive_name
-from rm_synthesis import rm_synth_main
+
 
 logger = logging.getLogger(__name__)
 
@@ -18,11 +19,12 @@ def archive_prof_fit(archive_file, ascii_outfile, kwargs=None):
     fit_dict = auto_gfit(profile, **kwargs)
     return fit_dict
 
+
 def RM_synth(cfg):
     archive_name = generate_archive_name(cfg)
-    archive_file = join(cfg["run_ops"]["psr_dir"], f"{archive_name}.ar")
-    ascii_outfile = join(cfg["run_ops"]["psr_dir"], f"{archive_name}.txt")
-    gfit_kwargs = {"cliptype":"verbose", "period":cfg["source"]["my_P"], "plot_name":f"{archive_name}.png"}
+    archive_file = cfg["files"]["archive"]
+    ascii_outfile = cfg["files"]["archive_ascii"]
+    gfit_kwargs = {"cliptype":"verbose", "period":cfg["source"]["my_P"], "plot_name":cfg["files"]["gfit_plot"]}
     # Do the fitting
     profile_fit = archive_prof_fit(archive_file, ascii_outfile, kwargs=gfit_kwargs)
     cfg["source"]["gfit"] = profile_fit
@@ -31,15 +33,15 @@ def RM_synth(cfg):
     rms_kwargs["phase_ranges"] = f"{profile_fit['component_1'][0]} {profile_fit['component_1'][1]}"
     rms_kwargs["phi_steps"] = 10000
     rms_kwargs["phi_range"] = (-300, 300)
-    rms_kwargs["label"] = f"{archive_name}_RMsynth_initial"
+    rms_kwargs["label"] = "RMsynth_initial"
     rms_kwargs["write"] = False
     rms_kwargs["plot"] = False
-    rms_kwargs["work_dir"] = cfg["run_ops"]["psr_dir"]
+    rms_kwargs["work_dir"] = cfg["files"]["psr_dir"]
     rm_dict = rm_synth_pipe(rms_kwargs)
     # Find the important things
     rm_inital = rm_dict["0"]["rm"]
     rms_kwargs["phi_range"] = (rm_initial-10, rm_final+10)
-    rms_kwargs["label"] = f"{archive_name}_RMsynth_final"
+    rms_kwargs["label"] = "RMsynth_final"
     rms_kwargs["write"] = True
     rms_kwargs["plot"] = True
     # RM synthesis - Fine
@@ -54,30 +56,30 @@ def RM_synth(cfg):
 def RM_cor(cfg, dpeends_on=None, depend_type="afterany"):
     """Submits an RM correction job"""
     # Create PSRSALSA commands
-    myfits = join(cfg['run_ops']['psr_dir'], f"{generate_archive_name(cfg)}.fits")
+    ar_name = generate_archive_name(cfg)
+    myfits = join(cfg['files']['psr_dir], f"{ar_name}.fits")
     on_pulse_min = cfg["source"]["gfit"]["component_1"][0]
-    on_pulse_max = cfg["source"]["gfit"]["component_1"][1]
-    outfile = join(cfg['run_ops']['psr_dir'], f"{generate_archive_name(cfg)}_rmcorrected.txt")
-    plotname = join(cfg['run_ops']['psr_dir'], f"{generate_archive_name(cfg)}_rmcorrected.ps")
+    on_pulse_max = cfg["source"]["gfit"]["component_1"][-1]
     rm = cfg["pol"]["RM"]
     ppol_cmd = "ppol -v"
     ppol_cmd += f" -onpulse '{on_pulse_min} {on_pulse_max}'"
     ppol_cmd += f" -header 'rm {rm}'"
     ppol_cmd += " -TSCR -FSCR"
-    ppol_cmd += f" -ofile {outifle}"
-    ppol_cmd += " -device /null -device2 /ps"
-    cmds = []
-    cmds.append(f"cd {cfg['run_ops']['psr_dir']}")
-    cmds.append(ppol_cmd)
+    ppol_cmd += f" -ext paswing"
+    ppol_cmd += f" -device {cfg['files']['ppol_profile_ps']}/cps" # is the profile
+    ppol_cmd += f" -device2 {cfg['files']['ppol_polar_profile_ps']}/cps" # is the polarimetry profile
+    ppol_cmd += f" {cfg['files']['debased_fits']}"
+    commands = [f"cd {cfg['files']['psr_dir]}"]
+    commands.append(ppol_cmd)
     # Submit the job
     name = f"{cfg['obs']['id']}_{cfg['source']['name']}_RM_correction"
     slurm_kwargs = {"time":"01:00:00"}
     modules = ["psrsalsa"]
     mem=8192
-    jid = submit_slurm(name, cmds,
-        slurm_kwargs=slurm_kwargs, module_list=modules, mem=mem, batch_dir=cfg["run_ops"]["batch_dir"], depend=depends_on,
+    jid = submit_slurm(name, commands,
+        slurm_kwargs=slurm_kwargs, module_list=modules, mem=mem, batch_dir=cfg["files"]["batch_dir"], depend=depends_on,
         depend_type=depend_type, vcstools_version=cfg["run_ops"]["vcstools"], submit=True)
-    logger.info(f"Submitted Rm correctoin creation job: {name}")
+    logger.info(f"Submitted Rm correction creation job: {name}")
     logger.info(f"job ID: {jid}")
     if depends_on:
         logger.info(f"Job depends on job id(s): {depends_on}")
