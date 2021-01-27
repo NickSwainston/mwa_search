5d32b010aea5bd8f6990a8dfd2bfba38530fbadf
keegansmith21 <keegan.r.s21@gmail.com>
2021-01-27 16:35:37 +0800

    Moved label location. Added singualrity module load. Fixed lavel counter

--- lib/dpp/helper_relaunch.py
+++ lib/dpp/helper_relaunch.py
@@ -5,11 +5,12 @@ from dpp.helper_config import dump_to_yaml
 
 logger = logging.getLogger(__name__)
 
+
 def launch_label(cfg):
     """Returns a label based on how far the piepline has progressed"""
-    order = ["Initial", "Classify", "Post", "Upload", "RM", "RVM_initial", "RVM_final"]
+    order = ["Initial", "Classify", "Post", "Upload", "Debase", "RM", "RVM_initial", "RVM_final"]
     counter = sum(cfg["completed"].values()) # Number of True statements
-    return order[counter]
+    return order[counter-1]
 
 
 def relaunch_ppp(cfg, depends_on=None, depend_type="afterany", fresh_run=False, reset_logs=False, time="00:30:00"):
@@ -17,7 +18,7 @@ def relaunch_ppp(cfg, depends_on=None, depend_type="afterany", fresh_run=False, 
     # dump the new cfg
     dump_to_yaml(cfg)
     label = launch_label(cfg)
-    name = f"ppp_{cfg['files']['file_precursor']}_{label}"
+    name = f"ppp_{label}_{cfg['files']['file_precursor']}"
     slurm_kwargs = {"time": time}
     mem=8192
     ppp_launch = "pulsar_processing_pipeline.py"
@@ -28,7 +29,7 @@ def relaunch_ppp(cfg, depends_on=None, depend_type="afterany", fresh_run=False, 
         ppp_launch += " --reset_logs"
     cmds = [f"cd {cfg['files']['psr_dir']}"]
     cmds.append(ppp_launch)
-    modules = [f"mwa_search/{cfg['run_ops']['mwa_search']}"]
+    modules = [f"mwa_search/{cfg['run_ops']['mwa_search']}", "singularity"]
     jid = submit_slurm(name, cmds,
             slurm_kwargs=slurm_kwargs, module_list=modules, mem=mem, batch_dir=cfg["files"]["batch_dir"], depend=depends_on,
             depend_type=depend_type, vcstools_version=cfg["run_ops"]["vcstools"], submit=True)
