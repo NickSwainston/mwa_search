b99fe03323f8934d91d3d9e3f5960774e319c68c
keegansmith21 <keegan.r.s21@gmail.com>
2021-02-11 12:32:31 +0800

    Changed how vdif processing is handled so that it now works as intended

--- lib/dpp/helper_archive.py
+++ lib/dpp/helper_archive.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from os.path import join
+from glob import glob
 
 from vcstools.prof_utils import auto_gfit, subprocess_pdv, get_from_ascii, ProfileLengthError, NoFitError
 from vcstools.job_submit import submit_slurm
@@ -30,9 +31,12 @@ def archive_fit(cfg, archive_path, cliptype="verbose"):
     cfg["source"]["gfit"] = fit
 
 
-def fits_to_archive(fits_dir, ar_name, bins, dm, period, memory=4000, total=1.0, seek=0, vdif=False, container="/pawsey/mwa/singularity/dspsr/dspsr.sif"):
+def fits_to_archive(fits_dir, ar_name, bins, dm, period, out_dir, memory=4000, total=1.0, seek=0, vdif=False, container="/pawsey/mwa/singularity/dspsr/dspsr.sif"):
     """Returns bash commands to fold on a fits file using dspsr"""
-    commands = []
+    # Normally I'd just use absolute filepaths but psrchive runs into issues if the filename 
+    # is too long. So instead we cd into the fits dir and process there, then move everything
+    # back to the out_dir
+    commands = [f"cd {fits_dir}"] 
     container_launch = (f"singularity exec -e {container}") # Open the container
     dspsr_cmd = f"{container_launch} dspsr  -A -K -cont"
     dspsr_cmd += f" -U {memory}"
@@ -42,18 +46,19 @@ def fits_to_archive(fits_dir, ar_name, bins, dm, period, memory=4000, total=1.0,
     dspsr_cmd += f" -S {seek}"
     dspsr_cmd += f" -T {total}"
     dspsr_cmd += f" -L {total}" # Timescrunch the whole obs
-    if vdif:
-        psradd_coms = f"psradd -R -m time *ar -o {ar_name}"
+    if vdif: # TODO: fix this because it can't deal with long filenames
         commands.append("j=0")
-        commands.append("for i in *.hdr;")
+        commands.append(f"for i in *.hdr;")
         commands.append(f"   do {dspsr_cmd} -O ipfb_$j $i ;")
         commands.append("   j=$((j+1))")
         commands.append("done;")
-        commands.append(psradd_coms)
+        commands.append(f"{container_launch} psradd -R -m time ipfb*.ar -o {ar_name}.ar")
     else:
         dspsr_cmd += f" -O {ar_name}"
-        dspsr_cmd += f" {fits_dir}/*.fits"
+        dspsr_cmd += f" *.fits"
         commands.append(dspsr_cmd)
+    commands.append(f"mv {ar_name}.ar {out_dir}")
+    commands.append("cd -")
     commands = "\n".join(commands) # Convert list to string
     return commands
 
@@ -82,17 +87,20 @@ def remove_baseline(cfg):
 def ppp_archive_creation(cfg, depends_on=None, depend_type="afterany"):
     """Makes commands for converting ot archive file then converting back to fits"""
     fits_dir = join(cfg["files"]["psr_dir"], cfg["source"]["my_pointing"])
-    bins = cfg["source"]["my_bins"]
+    bins = cfg["source"]["my_bins"] if not cfg["run_ops"]["vdif"] else 1024
     dm = cfg["source"]["my_DM"]
     period = cfg["source"]["my_P"]
     total = cfg["source"]["total"]
     seek = cfg["source"]["seek"]
     # Change to working directory
     commands = [f"cd {cfg['files']['psr_dir']}"]
+    # Check for vdif
+    vdif_hdrs = join(cfg["source"]["my_pointing"], "*.hdr")
+    cfg["run_ops"]["vdif"] = bool(glob(vdif_hdrs))
     # Add folds to commands
     psrchive_container = comp_config['prschive_container']
     archive_base = cfg["files"]["archive"].split(".ar")[0] # Archive without .ar extension
-    commands.append(fits_to_archive(fits_dir, archive_base, bins, dm, period,
+    commands.append(fits_to_archive(fits_dir, archive_base, bins, dm, period, cfg["files"]["psr_dir"],
                     total=total, seek=seek, vdif=cfg["run_ops"]["vdif"], container=psrchive_container))
     # Add ar -> fits conversion to commands
     commands.append(archive_to_fits(cfg["files"]["archive"], container=psrchive_container))
