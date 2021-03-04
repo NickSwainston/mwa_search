cea28695daaa62ce3c769760b10f9411b81cf288
keegansmith21 <keegan.r.s21@gmail.com>
2021-03-04 14:22:14 +0800

    Made the status dict global so that it can be referenced from anywhere

--- lib/dpp/helper_status.py
+++ lib/dpp/helper_status.py
@@ -8,14 +8,11 @@ from dpp.helper_config import from_yaml
 from dpp.helper_checks import check_file_dir_exists
 from dpp.helper_bestprof import NoUsableFoldsError
 from dpp.helper_checks import InvalidPAFileError, FitsNotFoundError, PFDNotFoundError
-from dpp.helper_classify import ClassifierFilesNotFoundError
+from dpp.helper_classify import ClassifierFilesNotFoundError, InvalidClassifyBinsError
 comp_config = load_config_file()
 logger = logging.getLogger(__name__)
 
-
-def message_from_status(status):
-    """Takes a status code and returns the defualt error message for it"""
-    status_dict = {
+STATUS_DICT = {
         "100": "Run completed with detection",
         "101": "Run completed but RVM could not be fit due to unsifficient paswing file",
         "102": "Profile has insufficient bins to continue with Gaussian fit",
@@ -26,9 +23,12 @@ def message_from_status(status):
         "203": "Bin count for classifer inputs is incorrect",
         "300": "Pipeline has not started",
         "400": "Something unknown went wrong"}
-    if status not in status_dict.keys():
-        raise ValueError(f"Invalid status: {status}. Valid statuses include: {status_dict.keys()}")
-    return(status_dict[status])
+
+def message_from_status(status):
+    """Takes a status code and returns the defualt error message for it"""
+    if status not in STATUS_DICT.keys():
+        raise ValueError(f"Invalid status: {status}. Valid statuses include: {STATUS_DICT.keys()}")
+    return(STATUS_DICT[status])
 
 def status_from_error(e):
     """Takes an error message and returns the designated status from it"""
@@ -64,13 +64,15 @@ def opp_status(obsid):
     check_file_dir_exists(dpp_dir)
     glob_cmd = join(dpp_dir, f"{obsid}*")
     psr_dirs = glob(glob_cmd)
-    status_dict = {"100":[], "101":[], "200":[], "201":[], "300":[]}
+    run_status = {}
+    for code in STATUS_DICT.keys():
+        run_status[code] = []
     for _dir in psr_dirs:
         status = cfg_status(_dir)
         psr = _dir.split("/")[-1].split("_")[-1]
         try:
-            status_dict[str(status)].append(psr)
+            run_status[str(status)].append(psr)
         except IndexError as e:
             logger.warn(f"Config file not found in directory: {psr_dir}")
             continue
-    return status_dict
+    return run_status
